4d6d092899c60c8e293503ecbcccb68b30900e92
Arghun-dev <arghun.developer@gmail.com>
2020-03-01 19:46:12 +0330

    Created Palette Component and passed seeColors.js as props to it

---
+++ colorapp/src/Palette.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Palette extends React.Component {
+    render() {
+        return (
+            <div className='Palette'>
+                {/* Navbar goes here */}
+                <div className='Palette-colors'>
+                    {/* bunch of color boxes */}
+                </div>
+                {/* footer goes here */}
+            </div>
+        )
+    }
+}
+
+export default Palette;
