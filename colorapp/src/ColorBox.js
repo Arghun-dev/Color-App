23989c75ea6819fd532dbf6c73a2d961460e2c6c
Arghun-dev <arghun.developer@gmail.com>
2020-03-01 21:36:02 +0330

    Added CopyToClipboard

--- colorapp/src/ColorBox.js
+++ colorapp/src/ColorBox.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './ColorBox.css';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 const ColorBox = ({name, color}) => {
     return (
-        <div style={{backgroundColor: `${color}`}} className='color-box'>
-            <span className='copy'>Copy</span>
-            <span className='name'>{name}</span>
-            <span className='more'>More</span>
-        </div>
+        <CopyToClipboard text={color}>
+            <div style={{backgroundColor: `${color}`}} className='color-box'>
+                <span className='copy'>Copy</span>
+                <span className='name'>{name}</span>
+                <span className='more'>More</span>
+            </div>
+        </CopyToClipboard>
     )
 }
 
