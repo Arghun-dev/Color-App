f1cbc2727ef42e380f50a76dd75d92fe9a07e1a9
Arghun-dev <arghun.developer@gmail.com>
2020-03-02 14:14:33 +0330

    Added React Router

--- colorapp/src/App.js
+++ colorapp/src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Palette from './Palette';
 import seedColors from './seedColors';
+import {Switch, Route} from 'react-router-dom';
 import './App.css';
 import { generatePalette } from './colorHelpers';
 
 function App() {
   return (
-    <div>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+
+    <Switch>
+      <Route exact path='/' render={() => <h1>PaletteList</h1>} />
+      <Route exact path='/palette/:id' render={() => <h1>Individual Palette</h1>} />
+    </Switch>
+
+    // <div>
+    //   <Palette palette={generatePalette(seedColors[4])} />
+    // </div>
   );
 }
 
